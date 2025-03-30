45158b1a0eb16e7ab114b6624914c1df498e8bfc
selvxhini-10 <selvahinik10@gmail.com>
2025-03-29 20:08:40 -0400

    Added Extension Sidebar Feature

--- popup.js
+++ popup.js
@@ -10,3 +10,22 @@ function extractText() {
     let text = document.body.innerText;
     chrome.runtime.sendMessage({ action: "summarize", text: text });
 }
+
+//Configures the side panel.
+chrome.sidePanel.setOptions({
+    // Whether sidepanel is enabled or not. By default it's true. 
+    enabled: true,
+    
+    // html file path
+    path: "popup.html",
+    
+    // the tab in which the sidepanel should appear. If omitted extensions assummes default behaviour.
+    // tabId: "tab-id" 
+  })
+
+  chrome.sidePanel.setPanelBehavior({
+    // Whether clicking the extension's icon will
+    // toggle showing the extension's entry 
+    // in the side panel. Defaults to false.
+    openPanelOnActionClick: true,
+ });
