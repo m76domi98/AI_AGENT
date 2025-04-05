cf3d900a6a1217f9c3d7e55680a9bf194f48380e
selvxhini-10 <selvahinik10@gmail.com>
2025-04-05 12:28:59 -0400

    Finalized Privacy Agent

--- llm.py
+++ llm.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY_CREDITS = {os.getenv("API_KEY"): 5}
+API_KEY_CREDITS = {os.getenv("API_KEY"): 10}
 app = FastAPI()
 
 class PromptRequest(BaseModel):
