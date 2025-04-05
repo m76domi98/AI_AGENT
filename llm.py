33d4f48e3c42c5f9330670c89ba9956c2a0561ea
selvxhini-10 <selvahinik10@gmail.com>
2025-04-05 12:34:14 -0400

    Modified API Credits

--- llm.py
+++ llm.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_KEY_CREDITS = {os.getenv("API_KEY"): 10}
+API_KEY_CREDITS = {os.getenv("API_KEY"): 20}
 app = FastAPI()
 
 class PromptRequest(BaseModel):
